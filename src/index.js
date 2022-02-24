de4235b8cd2a823ca4811af29a16595026a7f695
Sergey <88570614+SergeyGutman@users.noreply.github.com>
2022-02-24 13:41:09 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let res = [];
+  let currentConfig = '';
+  for (let i = 0; i < str.length; i++) {
+    if (res.length === 0) {
+      res.push(str[i]);
+    } else {
+      bracketsConfig.forEach(el => (el.includes(res[res.length - 1]) && (currentConfig = el)));
+      str[i] === currentConfig[1] ? res.pop() : res.push(str[i]);
+    }
+  }
+  return res.length > 0 ? false : true;
 }
